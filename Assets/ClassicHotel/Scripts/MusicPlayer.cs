3f94de6c9180baaecb2c2c152502e854d7d249ae
Darkziss <aserboom9@gmail.com>
2025-10-09 20:36:57 +0300

    Change MusicPlayer class

--- Assets/ClassicHotel/Scripts/MusicPlayer.cs
+++ Assets/ClassicHotel/Scripts/MusicPlayer.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 namespace ClassicHotel
 {
-    [RequireComponent(typeof(SpriteRenderer), typeof(AudioSource))]
+    [RequireComponent(typeof(MeshFilter), typeof(AudioSource))]
     public class MusicPlayer : MonoBehaviour
     {
-        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private MeshRenderer _meshRenderer;
 
         [SerializeField] private AudioSource _audioSource;
         [SerializeField] private AudioSource _clickAudioSource;
         
         [SerializeField] private AudioSource _ambienceAudioSource;
 
-        [SerializeField] private Sprite _enabledSprite;
-        [SerializeField] private Sprite _disabledSprite;
+        [SerializeField] private Color32 _screenEnabledColor = Color.white;
+        [SerializeField] private Color32 _screenDisabledColor = Color.black;
 
         [SerializeField] private AudioClip[] _clickAudioClips;
 
@@ -22,11 +22,13 @@ namespace ClassicHotel
         private const float NormalAmbienceVolume = 1f;
         private const float MuffledAmbienceVolume = 0.3f;
 
+        private const int ScreenMaterialIndex = 1;
+
         private void OnValidate()
         {
-            if (_spriteRenderer == null)
+            if (_meshRenderer == null)
             {
-                _spriteRenderer = GetComponent<SpriteRenderer>();
+                _meshRenderer = GetComponent<MeshRenderer>();
             }
 
             if (_audioSource == null)
@@ -46,7 +48,7 @@ namespace ClassicHotel
 
             PlayRandomClickSound();
 
-            _spriteRenderer.sprite = _enabledSprite;
+            _meshRenderer.sharedMaterials[ScreenMaterialIndex].color = Color.white;
 
             if (_audioSource.time > 0f)
             {
@@ -71,7 +73,8 @@ namespace ClassicHotel
 
             PlayRandomClickSound();
 
-            _spriteRenderer.sprite = _disabledSprite;
+            _meshRenderer.sharedMaterials[ScreenMaterialIndex].color = _screenDisabledColor;
+
             _audioSource.Pause();
 
             _ambienceAudioSource.volume = NormalAmbienceVolume;
