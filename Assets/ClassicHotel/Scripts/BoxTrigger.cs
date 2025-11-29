a868b2b245ce0c5c878eb914469d9f25cead75f9
Darkziss <aserboom9@gmail.com>
2025-11-29 21:42:41 +0300

    feature: add ending trigger

---
+++ Assets/ClassicHotel/Scripts/BoxTrigger.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+namespace ClassicHotel
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class BoxTrigger : MonoBehaviour
+    {
+        private BoxCollider _collider;
+        
+        public event Action TriggerEntered;
+
+        private void OnValidate()
+        {
+            if (_collider == null)
+            {
+                _collider = GetComponent<BoxCollider>();
+            }
+
+            if (!_collider.isTrigger)
+            {
+                _collider.isTrigger = true;
+            }
+        }
+
+        private void Awake()
+        {
+            TriggerEntered += () => Debug.LogError(gameObject.name);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            TriggerEntered?.Invoke();
+        }
+    }
+}
