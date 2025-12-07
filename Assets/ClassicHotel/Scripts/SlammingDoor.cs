f6fd0043cbc3e91f5a6823069716feda6680ce69
Darkziss <aserboom9@gmail.com>
2025-12-07 22:23:42 +0300

    feature: add logic to slam door

---
+++ Assets/ClassicHotel/Scripts/SlammingDoor.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using PrimeTween;
+
+namespace ClassicHotel
+{
+    public class SlammingDoor : MonoBehaviour
+    {
+        [SerializeField] private Transform _transform;
+        
+        [SerializeField] private BoxTrigger _trigger;
+
+        [SerializeField] private Vector3 _closedRotation;
+
+        private const float SlamDuration = 0.3f;
+        private const Ease SlamEase = Ease.Default;
+
+        private void OnValidate()
+        {
+            if (_transform == null)
+            {
+                _transform = transform;
+            }
+        }
+
+        private void OnEnable()
+        {
+            _trigger.TriggerEntered += Slam;
+        }
+
+        private void OnDisable()
+        {
+            _trigger.TriggerEntered -= Slam;
+        }
+
+        private void Slam()
+        {
+            TweenSettings<Vector3> settings = new(_closedRotation, SlamDuration, SlamEase);
+
+            Tween.LocalRotation(_transform, settings);
+        }
+    }
+}
