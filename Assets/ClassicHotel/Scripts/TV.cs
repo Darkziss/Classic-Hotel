9c66baff5f14c1fac710a7cfca6899b918d27be1
Darkziss <aserboom9@gmail.com>
2025-12-14 16:44:28 +0300

    feat: play sound on enable

--- Assets/ClassicHotel/Scripts/TV.cs
+++ Assets/ClassicHotel/Scripts/TV.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 namespace ClassicHotel
 {
+    [RequireComponent(typeof(AudioSource))]
     public class TV : MonoBehaviour
     {
         [SerializeField] private BoxTrigger _tvEnableTrigger;
+
+        [SerializeField] private AudioSource _audioSource;
+
+        [SerializeField] private AudioClip _switchSound;
+
+        private void OnValidate()
+        {
+            if (_audioSource == null)
+            {
+                _audioSource = GetComponent<AudioSource>();
+            }
+        }
 
         private void OnEnable()
         {
@@ -18,7 +31,7 @@ namespace ClassicHotel
 
         private void TriggerEvent()
         {
-            Debug.Log("TV Enabled");
+            _audioSource.PlayOneShot(_switchSound);
         }
     }
 }
