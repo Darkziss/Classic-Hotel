2640ebdae86970cb10011b203bf810120c9996d3
Darkziss <aserboom9@gmail.com>
2025-12-14 18:51:54 +0300

    feat: control emission of tv screen during event

--- Assets/ClassicHotel/Scripts/TV.cs
+++ Assets/ClassicHotel/Scripts/TV.cs
@@ -3,23 +3,43 @@ using UnityEngine;
 
 namespace ClassicHotel
 {
-    [RequireComponent(typeof(AudioSource))]
+    [RequireComponent(typeof(MeshRenderer), typeof(AudioSource))]
     public class TV : MonoBehaviour
     {
         [SerializeField] private BoxTrigger _tvEnableTrigger;
 
+        [SerializeField] private MeshRenderer _meshRenderer;
         [SerializeField] private AudioSource _audioSource;
+
+        [SerializeField, ColorUsage(true, true)] private Color _screenEnabledColor = Color.white;
+        [SerializeField] private Color _screenDisabledColor = Color.black;
 
         [SerializeField] private AudioClip _switchSound;
 
+        private Material _instancedScreenMaterial;
+
         private const float ProgramStartDelay = 0.5f;
+
+        private const int ScreenMaterialIndex = 1;
+
+        private const string EmissionColorPropertyName = "_EmissionColor";
 
         private void OnValidate()
         {
+            if (_meshRenderer == null)
+            {
+                _meshRenderer = GetComponent<MeshRenderer>();
+            }
+            
             if (_audioSource == null)
             {
                 _audioSource = GetComponent<AudioSource>();
             }
+        }
+
+        private void Start()
+        {
+            _instancedScreenMaterial = _meshRenderer.materials[ScreenMaterialIndex];
         }
 
         private void OnEnable()
@@ -45,16 +65,25 @@ namespace ClassicHotel
 
             PlaySwitchSound();
 
+            SetColorOfScreen(_screenEnabledColor);
+
             _audioSource.PlayDelayed(ProgramStartDelay);
 
             yield return disableDelay;
 
             PlaySwitchSound();
 
+            SetColorOfScreen(_screenDisabledColor);
+
             void PlaySwitchSound()
             {
                 _audioSource.PlayOneShot(_switchSound);
             }
+        }
+
+        private void SetColorOfScreen(Color color)
+        {
+            _instancedScreenMaterial.SetColor(EmissionColorPropertyName, color);
         }
     }
 }
