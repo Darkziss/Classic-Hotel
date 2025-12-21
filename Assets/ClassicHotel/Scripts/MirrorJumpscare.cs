3c5d58d00e73de628470f3e3ed28cf1ef1d4cd5d
Darkziss <aserboom9@gmail.com>
2025-12-21 16:36:45 +0300

    feat: add scaling and pause

--- Assets/ClassicHotel/Scripts/MirrorJumpscare.cs
+++ Assets/ClassicHotel/Scripts/MirrorJumpscare.cs
@@ -10,7 +10,9 @@ namespace ClassicHotel
         [SerializeField] private Transform _mirrorSphereTransform;
         [SerializeField] private Transform _jumpscarePointTransform;
 
-        [SerializeField] private TweenSettings _jumpscareTweenSettings;
+        [SerializeField] private Transform _sphereStartTransform;
+
+        [SerializeField] float _sphereTargetUniformScale;
 
         private void OnEnable()
         {
@@ -24,11 +26,28 @@ namespace ClassicHotel
 
         private void TriggerJumpscare()
         {
+            const float ScaleDuration = 1f;
+            const Ease ScaleEase = Ease.OutQuad;
+
+            const float StartPositionDuration = 0.5f;
+
+            const float JumpscareDelay = 1f;
+
+            const float JumpscareDuration = 0.25f;
+            const Ease JumpscareEase = Ease.InBack;
+
+            TweenSettings<float> scaleSettings = new(0f, _sphereTargetUniformScale, ScaleDuration, ScaleEase);
+            TweenSettings<Vector3> startPositionSettings = new(_sphereStartTransform.position, StartPositionDuration);
+
+            TweenSettings<Vector3> jumpscareSettings = new(_jumpscarePointTransform.position, JumpscareDuration, JumpscareEase);
+
             _mirrorSphereTransform.gameObject.SetActive(true);
 
-            TweenSettings<Vector3> tweenSettings = new(_jumpscarePointTransform.position, _jumpscareTweenSettings);
-
-            Tween.Position(_mirrorSphereTransform, tweenSettings)
+            Sequence.Create()
+                .Group(Tween.Scale(_mirrorSphereTransform, scaleSettings))
+                .Group(Tween.Position(_mirrorSphereTransform, startPositionSettings))
+                .ChainDelay(JumpscareDelay)
+                .Chain(Tween.Position(_mirrorSphereTransform, jumpscareSettings))
                 .OnComplete(() => _mirrorSphereTransform.gameObject.SetActive(false));
         }
     }
